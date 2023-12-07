2fb91e5af43db706949ae5aee0e6b79444d0ca61
Teru Kaazhooft <n401571@gymnasiumnovum.nl>
2023-12-07 13:25:50 +0100

    Initial commit

---
+++ game.py
@@ -0,0 +1,29 @@
+import pygame
+
+
+SCREEN_TILES = (12, 10)
+TILE_SIZE = 10
+
+
+class Game:
+
+    def __init__(self):
+
+        _ = pygame.display.set_mode((0, 0)) # fix bug on windows not going fullscreen
+
+        self.display = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+        self.screen = pygame.Surface((SCREEN_TILES[0] * TILE_SIZE, SCREEN_TILES[1] * TILE_SIZE))
+
+    def update(self, events: list[pygame.event.Event]):
+        for event in events:
+            pass
+
+    def draw(self):
+        self.display.fill((0, 0, 0))
+
+        screen = self.screen
+        screen.fill((255, 0, 0))
+
+        self.display.blit(screen, (0, 0))
+
+        pygame.display.update()
---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+import game
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+game = game.Game()
+run = True
+
+while run:
+
+    clock.tick(60)
+
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+        elif event.type == pygame.QUIT:
+            run = False
+
+    game.update(events)
+    game.draw()
+
+pygame.quit()
